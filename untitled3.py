791a4165250d2b6d2272b1add694e33e65b826b1
Setareh <setareh10@gmail.com>
2021-03-19 12:36:47 +0000

    Update: 19/03/21

--- untitled3.py
+++ untitled3.py
@@ -13,7 +13,7 @@ from sklearn.model_selection import (cross_validate, KFold)
 x = np.arange(0.0, 10, 0.01).reshape(-1, 1)
 y = np.sin(2 * np.pi * x).ravel()
 
-nn = MLPRegressor(hidden_layer_sizes=(3), 
+nn = MLPRegressor(hidden_layer_sizes=(3),
                   activation='tanh', solver='lbfgs')
 
 n = nn.fit(x, y)
@@ -22,14 +22,14 @@ test_y = nn.predict(test_x)
 
 kf = KFold(n_splits=10)
 
-scores =cross_validate(
-            nn, x, y, scoring=('explained_variance'), cv=kf, n_jobs=-1)
+scores = cross_validate(
+    nn, x, y, scoring=('explained_variance'), cv=kf, n_jobs=-1)
 score = np.mean(scores['test_score'])
 
 fig = plt.figure()
 ax1 = fig.add_subplot(111)
 ax1.scatter(x, y, s=5, c='b', marker="o", label='real')
-ax1.plot(test_x,test_y, c='r', label='NN Prediction')
+ax1.plot(test_x, test_y, c='r', label='NN Prediction')
 
 plt.legend()
 plt.show()
