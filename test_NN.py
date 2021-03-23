d57177c35cbc2493e6d0899af3e05f985c07a56c
Setareh <setareh10@gmail.com>
2021-03-23 13:12:42 +0000

    modified

--- test_NN.py
+++ test_NN.py
@@ -11,57 +11,127 @@ import matplotlib.pyplot as plt
 from sklearn.neural_network import MLPRegressor
 from sklearn.linear_model import RidgeCV
 from sklearn.model_selection import (cross_validate, KFold)
+from sklearn.metrics import mean_squared_error, r2_score, explained_variance_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
+############################################################################
+# X = np.arange(0.0, 1, 0.01).reshape(-1, 1)
+# y = np.sin(2 * np.pi * X).ravel()
+# # X = np.arange(0,100,1).reshape(-1, 1)
+# # y = .8*X
+# n_splits = 10
 
-r, c = [200, 100]
+# kf = KFold(n_splits=n_splits)
+# MSE_s = np.zeros([n_splits])
+# var_s = np.zeros([n_splits])
+# activation = 'tanh'
+
+# for s, (train, test) in enumerate(kf.split(X, y)):
+#     # print(s,train, test)
+#     nn = MLPRegressor(hidden_layer_sizes=(3),
+#                       activation=activation, solver='lbfgs')
+#     nn.fit(X[train], y[train])
+#     y_pred_CV = nn.predict(X[test])
+
+#     fig = plt.figure()
+#     ax1 = fig.add_subplot(111)
+#     ax1.scatter(X, y, s=5, c='b', marker="o", label='real')
+#     ax1.plot(X[test], y_pred_CV, c='r', label='NN Prediction')
+
+#     plt.legend()
+#     plt.show()
+
+#     MSE_s[s] = mean_squared_error(y[test], y_pred_CV)
+#     var_s[s] = explained_variance_score(y[test], y_pred_CV)
+# print( np.mean(var_s))
+
+# nn = MLPRegressor(hidden_layer_sizes=(3),
+#                   activation=activation, solver='lbfgs')
+# scores = cross_validate(
+#     nn, X, y, scoring=('explained_variance'), cv=kf, n_jobs=-1)
+# score = np.mean(scores['test_score'])
+# print(score)
+
+###########################################################################
+normalize = True
+r, c = [200, 50]
 y1 = np.zeros([r, c])
-hidden_layer_size = np.arange(int(r/5), int(3*r), 50)
-# [int(r/5), int(r/2), int(r),
-#                      int(1.5*r), int(2*r), int(3*r), int(5*r), int(10*r)]
-noise = np.random.normal(0, .1, (r, c))
+hidden_layer_sizes = 100
+
+noise = np.random.normal(0, 1, (r, c))
 T = np.random.normal(0, 1, (c, c))
-X = np.random.normal(0, 1, (r, c))
-# y = np.matmul(X, T) + noise
-for i in np.arange(0, r):
-    for j in np.arange(0, c):
-        y1[i, j] = math.tanh(X[i, j])
-y = y1 + noise
+X1 = np.random.normal(0, 1, (r, c))
+y1 = np.matmul(X1, T)  # + noise
+# y1 = np.sin(X1)
+
+# y1 = y1 + noise
+# scalerx = StandardScaler()
+# scalery = StandardScaler()
+
+# scalerx.fit(X1)
+# scalery.fit(y1)
+
+# X = scalerx.transform(X1)
+# y = scalerx.transform(y1)
+
 
 # y = np.matmul(X, T)
-print('SNR: ', np.var(y1)/np.var(noise))
-score_nh = np.zeros([len(hidden_layer_size), 1])
+# print('SNR: ', np.var(y1)/np.var(noise))
+# score_nh = np.zeros([len(hidden_layer_size), 1])
 
-r = X.shape[0]
+r = X1.shape[0]
 if (r/5) > 10:
     n_splits = 10
 else:
     n_splits = 5
 
 kf = KFold(n_splits=n_splits)
-regrCV = RidgeCV(alphas=np.logspace(-5, 5, 100), normalize=True)
-scoresCV = cross_validate(
-    regrCV, X, y, scoring=('explained_variance'), cv=kf, n_jobs=-1)
-score_CV = np.mean(scoresCV['test_score'])
-print('RR score: ', score_CV)
-# for i, nh in enumerate(hidden_layer_size):
-#     # for nh in [40,80,120,200,1000]:
+MSE_s = np.zeros([n_splits])
+var_s = np.zeros([n_splits])
+activation = 'identity'
 
-#     print('hidden_layer_size: ', nh)
-#     regrMLP = MLPRegressor(hidden_layer_sizes=(nh), max_iter=500,
-#                            activation='identity', solver='lbfgs')
-#     scoresMLP = cross_validate(
-#         regrMLP, X, y, scoring=('explained_variance'), cv=kf, n_jobs=-1)
-#     score_nh[i, 0] = np.mean(scoresMLP['test_score'])
-#     print('MPL score: ', np.mean(scoresMLP['test_score']))
+for s, (train, test) in enumerate(kf.split(X1, y1)):
+    # print(s,train, test)
+    mlp = make_pipeline(StandardScaler(),
+                        MLPRegressor(hidden_layer_sizes=hidden_layer_sizes,
+                                     activation=activation, solver='lbfgs', max_iter=1000))
+    # nn = MLPRegressor(hidden_layer_sizes=(100),
+    #                   activation=activation, solver='lbfgs', max_iter=1000)
+    mlp.fit(X1[train, :], y1[train, :])
+    y_pred = mlp.predict(X1[test, :])
 
-# plt.figure()
-# plt.plot(hidden_layer_size, score_nh)
+    # fig = plt.figure()
+    # ax1 = fig.add_subplot(111)
+    # ax1.scatter(X1[test, 0], y1[test, 0], s=5, c='b', marker="s", label='real')
+    # ax1.scatter(X1[test, 0], y_pred[:, 0], c='r',
+    #             marker="o", label='NN Prediction')
 
-regrMLP = MLPRegressor(hidden_layer_sizes=r+1000, max_iter=500,
-                       activation='tanh', solver='lbfgs')
-scoresMLP = cross_validate(
-    regrMLP, X, y, scoring=('explained_variance'), cv=kf, n_jobs=-1)
-print('MPL score: ', np.mean(scoresMLP['test_score']))
+    # plt.legend()
+    # plt.show()
 
-# plt.figure()
-# plt.plot(hidden_layer_size, score_nh)
+    MSE_s[s] = mean_squared_error(y1[test, 0], y_pred[:, 0])
+    var_s[s] = explained_variance_score(y1[test, 1], y_pred[:, 1])
+
+
+print(np.mean(var_s))
+
+nn = MLPRegressor(hidden_layer_sizes=hidden_layer_sizes,
+                  activation=activation, solver='lbfgs', max_iter=1000)
+scores = cross_validate(
+    nn, X1, y1, scoring=('explained_variance'), cv=kf, n_jobs=-1)
+score = np.mean(scores['test_score'])
+print(score)
+
 # plt.close('all')
+
+
+# from keras.models import Sequential
+# from keras.layers import Dense
+# model = Sequential()
+# input_size = len(X[0])
+# model.add(Dense(200, activation="relu", input_dim=input_size))
+# model.add(Dense(200, activation="relu"))
+# model.add(Dense(1, activation="linear"))
+# model.compile(optimizer="adam", loss="mse")
+# model.fit(X_train, y_train, epochs=25, verbose=1)
+# y_pred = model.predict(X_test)
--- untitled3.py
+++ untitled3.py
@@ -9,27 +9,8 @@ from sklearn.neural_network import MLPRegressor
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import (cross_validate, KFold)
+from sklearn.metrics import mean_squared_error, r2_score, explained_variance_score
 
-x = np.arange(0.0, 10, 0.01).reshape(-1, 1)
-y = np.sin(2 * np.pi * x).ravel()
 
-nn = MLPRegressor(hidden_layer_sizes=(3),
-                  activation='tanh', solver='lbfgs')
 
-n = nn.fit(x, y)
-test_x = np.arange(-0.1, 1.1, 0.01).reshape(-1, 1)
-test_y = nn.predict(test_x)
-
-kf = KFold(n_splits=10)
-
-scores = cross_validate(
-    nn, x, y, scoring=('explained_variance'), cv=kf, n_jobs=-1)
-score = np.mean(scores['test_score'])
-
-fig = plt.figure()
-ax1 = fig.add_subplot(111)
-ax1.scatter(x, y, s=5, c='b', marker="o", label='real')
-ax1.plot(test_x, test_y, c='r', label='NN Prediction')
-
-plt.legend()
-plt.show()
+# plt.close('all')
